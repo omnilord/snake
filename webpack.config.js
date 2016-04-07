d5b62d72b7a969c61a7913021451898b43c90803
Omnilord <omnilord@privateerconsulting.com>
2016-04-07 03:34:31 +0000

    Initial completion of webpack setup such that a bundle.js is producible.

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './src/app.jsx',
+  devtool: 'source-map',
+  output: { filename: 'bundle.min.js' },
+  resolve: { extensions: ['', '.js', '.jsx'] },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: { presets: ['es2015', 'react'] }
+    },{
+      test: /\.sass?$/,
+      exclude: /node_modules/,
+      loader: 'sass',
+    }]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({minimize: true})
+  ]
+};
